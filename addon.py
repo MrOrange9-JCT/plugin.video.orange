bcfe4411b1249871996312b46f18f8c402a210c0
MrOrange9-JCT <mrorange9.jct@gmail.com>
2023-12-23 14:24:29 +0100

    Created addon.xml and addon.py

---
+++ addon.py
@@ -0,0 +1,13 @@
+import sys
+import xbmcgui
+import xbmcplugin
+
+addon_handle = int(sys.argv[1])
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+url = 'file:///D:/Plex/Películas/Matrix%20(1999)/Matrix%201%20-%20H265.mp4'
+li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+xbmcplugin.endOfDirectory(addon_handle)
