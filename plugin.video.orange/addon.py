5a7b973fe27fe1690c420a023debe01642c8e71e
MrOrange9-JCT <mrorange9.jct@gmail.com>
2023-12-24 13:59:12 +0100

    Added requests for movie_list and metadata from TMDB

--- plugin.video.orange/addon.py
+++ plugin.video.orange/addon.py
@@ -2,28 +2,44 @@ import sys
 import xbmc
 import xbmcgui
 import xbmcplugin
+import requests
+import simplejson as json
 
 addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, "movies")
 
-def getMovieMetadata(imdb_id, key):
-    info = xbmc.InfoTagVideo()
+def getMovieMetadata(movie_title, requested_metadata):
 
-    info.setIMDBNumber(imdb_id)
-    metadata = {"title": info.getTitle(),
-                "year": info.getYear(),
-                "genre": info.getGenres(),
-                "rating": info.getRating("imdb"),
-                "plot": info.getPlot()}
+    tmdb_id = movie_list[movie_title]
+    api_key = "-706d6c75628138ee3084133305f15bf6-ee30841333e3084133305f15bf6"
+
+    url = f"https://api.themoviedb.org/3/movie/{tmdb_id}?language=es-ES&api_key={api_key.split('-')[1]}"
+    response = requests.get(url)
+    r = response.json()
+
+    metadata = {"title": r["title"],
+                "year": r["release_date"].split("-")[0],
+                "genres": r["genres"],
+                "rating": r["vote_average"],
+                "duration": r["runtime"] + " min",
+                "tagline": r["tagline"],
+                "plot": r["overview"],}
     
-    return metadata[key]
+    return metadata[requested_metadata]
 
-# Movies along with their IMDB ID
-movies_list = {"matrix": "tt0133093"}
+def getMovieList():
+    """"Get the updated movie list from Rentry.co"""
+    url = "https://rentry.co/OrangeAddon_movie_list/raw"
+    response = requests.get(url)
 
-movie = xbmcgui.ListItem(f"{getMovieMetadata(movies_list['matrix'], 'title')} [COLOR blue]({getMovieMetadata(movies_list['matrix'], 'year')})[/COLOR]")
-#movie.setInfo("video", {})
+    return response.json()
+
+# Movies along with their TMDB ID
+movie_list = getMovieList()
+
+movie = xbmcgui.ListItem(f"{getMovieMetadata('matrix', 'title')} [COLOR blue]({getMovieMetadata('matrix', 'year')})[/COLOR]")
+movie.setInfo("video", {})
 
 movie.setArt({"poster": "https://www.themoviedb.org/t/p/original/qK76PKQLd6zlMn0u83Ej9YQOqPL.jpg", 
                "fanart": "https://www.themoviedb.org/t/p/original/y0jFVsgbvPE3AJqNxIPRhM7pWrO.jpg"})
