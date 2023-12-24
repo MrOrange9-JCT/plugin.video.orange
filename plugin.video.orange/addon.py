090094b68446da16e0462e5e36cf10ad413db50e
MrOrange9-JCT <mrorange9.jct@gmail.com>
2023-12-24 18:30:50 +0100

    Added metadata to the list_items

--- plugin.video.orange/addon.py
+++ plugin.video.orange/addon.py
@@ -5,6 +5,7 @@ import xbmcplugin
 import requests
 
 addon_handle = int(sys.argv[1])
+__url__ = sys.argv[0]
 
 xbmcplugin.setContent(addon_handle, "movies")
 
@@ -18,10 +19,10 @@ def getMovieMetadata(movie_title, requested_metadata):
     r = response.json()
 
     metadata = {"title": r["title"],
-                "year": r["release_date"].split("-")[0],
+                "year": int(r["release_date"].split("-")[0]),
                 "genres": r["genres"],
-                "rating": str(r["vote_average"]),
-                "duration": str(r["runtime"]) + " min",
+                "rating": r["vote_average"],
+                "duration": r["runtime"] * 60,
                 "tagline": r["tagline"],
                 "plot": r["overview"],
                 "poster": "https://www.themoviedb.org/t/p/original" + r["poster_path"],
@@ -41,18 +42,26 @@ def updateMovieList():
     url = "https://rentry.co/OrangeAddon_movie_list/raw"
     response = requests.get(url)
 
-    return response.json()
+    global movie_list
+    movie_list = response.json()
+
+    return __url__ + "/update"
 
 # Movies along with their TMDB ID
 movie_list = getMovieList()
 
-update_list = xmbcgui.ListItem("[COLOR green] - Actualizar lista de películas - [/COLOR]")
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=updateMovieList(), listitem=update_list)
-
+update_list = xbmcgui.ListItem("[COLOR lime] - Actualizar lista de películas - [/COLOR]")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=updateMovieList(), listitem=update_list, isFolder=True)
 
 for movie in movie_list:
     list_item = xbmcgui.ListItem(f"{getMovieMetadata(movie, 'title')} [COLOR blue]({getMovieMetadata(movie, 'year')})[/COLOR]")
-    list_item.setInfo("video", {})
+    list_item.setInfo("video", {"genre": getMovieMetadata(movie, 'genres'),
+                                "rating": getMovieMetadata(movie, 'rating'),
+                                "duration": getMovieMetadata(movie, 'duration'),
+                                "plotoutline": getMovieMetadata(movie, 'tagline'),
+                                "plot": getMovieMetadata(movie, 'plot'),
+                                "year": getMovieMetadata(movie, 'year'),
+                                "title": getMovieMetadata(movie, 'title'),})
 
     list_item.setArt({"poster": getMovieMetadata(movie, 'poster'), 
                 "fanart": getMovieMetadata(movie, 'fanart')})
