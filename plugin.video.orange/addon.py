dd91ddd7a94ea1c3cf61dbdd38977683bafae42f
MrOrange9-JCT <mrorange9.jct@gmail.com>
2023-12-24 14:54:17 +0100

    Added loop to iterate through the current movie list and a update function

--- plugin.video.orange/addon.py
+++ plugin.video.orange/addon.py
@@ -3,7 +3,6 @@ import xbmc
 import xbmcgui
 import xbmcplugin
 import requests
-import simplejson as json
 
 addon_handle = int(sys.argv[1])
 
@@ -11,7 +10,7 @@ xbmcplugin.setContent(addon_handle, "movies")
 
 def getMovieMetadata(movie_title, requested_metadata):
 
-    tmdb_id = movie_list[movie_title]
+    tmdb_id = movie_list[movie_title][0]
     api_key = "-706d6c75628138ee3084133305f15bf6-ee30841333e3084133305f15bf6"
 
     url = f"https://api.themoviedb.org/3/movie/{tmdb_id}?language=es-ES&api_key={api_key.split('-')[1]}"
@@ -21,8 +20,8 @@ def getMovieMetadata(movie_title, requested_metadata):
     metadata = {"title": r["title"],
                 "year": r["release_date"].split("-")[0],
                 "genres": r["genres"],
-                "rating": r["vote_average"],
-                "duration": r["runtime"] + " min",
+                "rating": str(r["vote_average"]),
+                "duration": str(r["runtime"]) + " min",
                 "tagline": r["tagline"],
                 "plot": r["overview"],
                 "poster": "https://www.themoviedb.org/t/p/original" + r["poster_path"],
@@ -37,17 +36,29 @@ def getMovieList():
 
     return response.json()
 
+def updateMovieList():
+    """Update the movie list on Rentry.co"""
+    url = "https://rentry.co/OrangeAddon_movie_list/raw"
+    response = requests.get(url)
+
+    return response.json()
+
 # Movies along with their TMDB ID
 movie_list = getMovieList()
 
-movie = xbmcgui.ListItem(f"{getMovieMetadata('matrix', 'title')} [COLOR blue]({getMovieMetadata('matrix', 'year')})[/COLOR]")
-movie.setInfo("video", {})
+update_list = xmbcgui.ListItem("[COLOR green] - Actualizar lista de películas - [/COLOR]")
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=updateMovieList(), listitem=update_list)
 
-movie.setArt({"poster": getMovieMetadata('matrix', 'poster'), 
-              "fanart": getMovieMetadata('matrix', 'fanart')})
 
-url = "D:\Plex\Películas\Matrix (1999)\Matrix 1 - H265.mp4"
+for movie in movie_list:
+    list_item = xbmcgui.ListItem(f"{getMovieMetadata(movie, 'title')} [COLOR blue]({getMovieMetadata(movie, 'year')})[/COLOR]")
+    list_item.setInfo("video", {})
 
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=movie)
+    list_item.setArt({"poster": getMovieMetadata(movie, 'poster'), 
+                "fanart": getMovieMetadata(movie, 'fanart')})
+
+    url = movie_list[movie][1]
+
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=list_item)
 
 xbmcplugin.endOfDirectory(addon_handle)
